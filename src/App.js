f25f8b610bd001b1aea3e9123bb33c06dab8c869
Jani Sarja <jani.sarja@live.com>
2023-12-16 12:45:40 +0200

    Change render to createRoot in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { Footer, Header, WeatherInfo } from './containers';
 
-const apiKey = '9dd31d462321665a6103dba10ebe21d0';
-
 const App = () => {
-
-
   return (
     <div className='App'>
       <div className='gradient__top'>
